8eda7e8a3ce339e2ca7abaafe997a13e545f670d
Sudipta Majumder <majumdersudipta27@gmail.com>
2024-08-17 18:32:20 +0530

    Removed deprecated MongoDB options from connection code

--- Backend/index.js
+++ Backend/index.js
@@ -25,13 +25,8 @@ const PORT = process.env.PORT || 5000;
 const URI = process.env.ATLASDB_URL;
 
 // Connect to MongoDB
-mongoose.connect(URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(error => console.error('Error connecting to MongoDB:', error));
-
+mongoose.connect(URI);
+ 
 // Define routes
 app.use('/codsoft', codsoftRoute);
 app.use('/user', userRoute);
