1158ce8027a0576d2b2725216b5d5611a1ce3ab0
Sudipta Majumder <143156739+SudiptaMajumder27@users.noreply.github.com>
2025-10-11 21:34:40 +0530

    Update jobpost.model.js

--- Backend/model/jobpost.model.js
+++ Backend/model/jobpost.model.js
@@ -1,11 +1,6 @@
 import mongoose from "mongoose";
 
-<<<<<<< HEAD
-const JobPostSchema = new mongoose.Schema(
-    {
-=======
 const JobPostSchema = new mongoose.Schema({
->>>>>>> 5cc42f323745e5990ee4a9bd6e8a425c4316d505
     email: { type: String, required: true },
     fullname: { type: String, required: true },
     company_name: { type: String, required: true },
@@ -18,12 +13,7 @@ const JobPostSchema = new mongoose.Schema({
     description: { type: String, required: true },
     imageUrl: { type: String, required: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-<<<<<<< HEAD
-}
-);
-=======
 });
->>>>>>> 5cc42f323745e5990ee4a9bd6e8a425c4316d505
 
 const JobPost = mongoose.model("jobposts", JobPostSchema);
 
